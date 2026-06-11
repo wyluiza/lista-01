520c9edd4bbcfe7227b0e50efc6683f9bbac3a3a
Luiza <luiza_knop@estudante.sesisenai.org.br>
2026-06-11 19:53:51 -0300

    Atividade

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Informe a idade do visitante: ");
+int idade = int.Parse(Console.ReadLine());
+if (idade >= 16)
+{
+    Console.WriteLine("Entrada permitida");
+}
+else
+{
+    Console.WriteLine("Entrada permitida somente acompanhado de um responsável.");
+}
