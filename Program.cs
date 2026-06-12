32a175ba8587f4c6f6873ff9028b6984d55b4bfb
Luiza <luiza_knop@estudante.sesisenai.org.br>
2026-06-11 22:19:18 -0300

    q5

--- Program.cs
+++ Program.cs
@@ -1,19 +1,12 @@
-﻿double valorCompra, desconto, valorFinal; //cria variável
-
-        Console.Write("Informe o valor total da compra: R$ ");
-        valorCompra = double.Parse(Console.ReadLine());
-
-        if (valorCompra >= 200)
-        {
-            desconto = (valorCompra * 10) / 100;
-        }
-        else
-        {
-            desconto = 0;
-        }
-
-        valorFinal = valorCompra - desconto;
-
-        Console.WriteLine("\nValor original: R$ " + valorCompra);
-        Console.WriteLine("Desconto aplicado: R$ " + desconto);
-        Console.WriteLine("Valor final: R$ " + valorFinal);
+﻿Console.WriteLine("Informe a quantidade de tarefas concluídas: ");
+int tarefas = int.Parse(Console.ReadLine());
+if (tarefas >= 50)
+{
+    Console.WriteLine("Meta atingida!");
+}
+else
+{
+    Console.WriteLine("Meta não atingida.");
+    tarefas = 50 - tarefas;
+    Console.WriteLine("Você precisa concluir mais {0} tarefas para atingir a meta.", tarefas);
+}
