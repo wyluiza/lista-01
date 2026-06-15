1a86230e8a03aa6e4ec3bff5332cdec41c3c4290
Luiza <luiza_knop@estudante.sesisenai.org.br>
2026-06-15 19:36:07 -0300

    q8

--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
-﻿Console.WriteLine("Informe o primeiro número: ");
-int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Informe o segundo número: ");
-int num2 = int.Parse(Console.ReadLine());
-if (num1 > num2)
-{
-    Console.WriteLine($"O primeiro número é maior");
-}
-else if (num2 > num1)
-{
-    Console.WriteLine($"O segundo número é maior");
-}
-else
-{
-    Console.WriteLine("Os números são iguais.");
-}
+﻿    Console.WriteLine("Informe a primeira pontuação: ");
+    int pontuacao1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe a segunda pontuação: ");
+    int pontuacao2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Informe a terceira pontuação: ");
+    int pontuacao3 = int.Parse(Console.ReadLine());
+
+    if (pontuacao1 > pontuacao2 && pontuacao1 > pontuacao3)
+    {
+ Console.WriteLine("A maior pontuação é: " + pontuacao1);
+    }
+    else if (pontuacao2 > pontuacao1 && pontuacao2 > pontuacao3)
+    {
+        Console.WriteLine("A maior pontuação é: " + pontuacao2);
+    }
+    else if (pontuacao3 > pontuacao1 && pontuacao3 > pontuacao2)
+    {
+        Console.WriteLine("A maior pontuação é: " + pontuacao3);
+    }
+    else
+    {
+        Console.WriteLine("As três pontuações são iguais.");
+    }
