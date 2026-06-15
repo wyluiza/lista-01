0f84bff0433218b5c7ac9666668c2006d7bc770a
Luiza <luiza_knop@estudante.sesisenai.org.br>
2026-06-15 20:45:44 -0300

    q19

--- Program.cs
+++ Program.cs
@@ -1,17 +1,50 @@
-﻿Console.WriteLine("Informe a quantidade de clientes atendidos: ");
-int clientes = int.Parse(Console.ReadLine());
+﻿using System;
 
-int tempoTotal = 0;
-
-for (int i = 1; i <= clientes; i++)
+class Program
 {
-    Console.WriteLine("Informe o tempo do atendimento: ");
-    int tempo = int.Parse(Console.ReadLine());
+    static void Main()
+    {
+        Console.WriteLine("Informe o saldo inicial:");
+        float saldo = float.Parse(Console.ReadLine());
 
-    tempoTotal = tempoTotal + tempo;
+        int operacao;
+
+        do
+        {
+            Console.WriteLine("Escolha a operação:");
+            Console.WriteLine("1 - Entrada de dinheiro");
+            Console.WriteLine("2 - Saída de dinheiro");
+            Console.WriteLine("3 - Encerrar");
+
+            operacao = int.Parse(Console.ReadLine());
+
+            if (operacao == 1)
+            {
+                Console.WriteLine("Informe o valor da movimentação:");
+                float valor = float.Parse(Console.ReadLine());
+
+                saldo = saldo + valor;
+
+                Console.WriteLine("Entrada registrada. Saldo atual: R$ " + saldo);
+            }
+            else if (operacao == 2)
+            {
+                Console.WriteLine("Informe o valor da movimentação:");
+                float valor = float.Parse(Console.ReadLine());
+
+                if (valor <= saldo)
+                {
+                    saldo = saldo - valor;
+                    Console.WriteLine("Saída registrada. Saldo atual: R$ " + saldo);
+                }
+                else
+                {
+                    Console.WriteLine("Saldo insuficiente.");
+                }
+            }
+
+        } while (operacao != 3);
+
+        Console.WriteLine("Saldo final: R$ " + saldo);
+    }
 }
-
-float tempoMedio = (float)tempoTotal / clientes;
-
-Console.WriteLine("Tempo total de atendimento: " + tempoTotal + " minutos");
-Console.WriteLine("Tempo médio por cliente: " + tempoMedio + " minutos");
