dcad709eaf44f4f15ee7e4eaebbc5491d91f35f0
Luiza <luiza_knop@estudante.sesisenai.org.br>
2026-06-15 20:05:16 -0300

    q16

--- Program.cs
+++ Program.cs
@@ -1,26 +1,9 @@
-﻿int positivos = 0;
-int negativos = 0;
-int zeros = 0;
-
-for (int i = 1; i <= 10; i++)
+﻿Console.WriteLine("Informe a senha: ");
+int senha = int.Parse(Console.ReadLine());
+while (senha != 1234)
 {
-    Console.WriteLine("Informe um número: ");
-    int numero = int.Parse(Console.ReadLine());
-
-    if (numero > 0)
-    {
-        positivos++;
-    }
-    else if (numero < 0)
-    {
-        negativos++;
-    }
-    else
-    {
-        zeros++;
-    }
+    Console.WriteLine("Senha incorreta. Tente novamente.");
+    Console.WriteLine("Informe a senha: ");
+    senha = int.Parse(Console.ReadLine());
 }
-
-Console.WriteLine("Quantidade de positivos: " + positivos);
-Console.WriteLine("Quantidade de negativos: " + negativos);
-Console.WriteLine("Quantidade de zeros: " + zeros);
+Console.WriteLine("Acesso permitido."); 
